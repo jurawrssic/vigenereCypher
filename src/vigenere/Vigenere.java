169f754a67a010e7843b6c2ff4e95fa373cffbc6
jurawrssic <juliardgs@yahoo.com.br>
2019-12-03 14:22:28 -0300

    First Commit

---
+++ src/vigenere/Vigenere.java
@@ -0,0 +1,88 @@
+package vigenere;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Julia
+ */
+public class Vigenere {  
+    private String msg, chave;
+    private String m[][] = {{"banana", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"},
+                            {"A", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"},
+                            {"B", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A"},
+                            {"C", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B"},
+                            {"D", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C"},
+                            {"E", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D"},
+                            {"F", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E"},
+                            {"G", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F"},
+                            {"H", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G"},
+                            {"I", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H"},
+                            {"J", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I"},
+                            {"K", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"},
+                            {"L", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"},
+                            {"M", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"},
+                            {"N", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"},
+                            {"O", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"},
+                            {"P", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"},
+                            {"Q", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"},
+                            {"R", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q"},
+                            {"S", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"},
+                            {"T", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"},
+                            {"U", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"},
+                            {"V", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U"},
+                            {"W", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"},
+                            {"X", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"},
+                            {"Y", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"},
+                            {"Z", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"}};
+    
+    public Vigenere() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escreva a mensagem que gostaria de criptografar SEM espaços!");
+        this.msg = sc.nextLine();
+        System.out.println("Agora insira a chave de codificação:");
+        this.chave = sc.nextLine(); 
+        chave = Ajustar(msg, chave);
+        Codificar(msg, chave);
+    } 
+    
+    public static String Ajustar(String msg, String chave){
+        String chaveAjustada = "";
+        int i=0;
+        do{
+            if(i == chave.length()){
+                i = 0;
+            }
+            chaveAjustada = chaveAjustada + chave.charAt(i);                     
+            i++;
+        }while(chaveAjustada.length() != msg.length());
+        
+        System.out.println("Sua chave ajustada ficou:");
+        System.out.println(chaveAjustada);
+        return chaveAjustada;
+    }
+
+    
+    public void Codificar(String msg, String chave){
+        String msgCodificada = "";
+        int x=0, y=0;                
+        for(int c=0; c<chave.length(); c++){
+            for(int i=0; i<27; i++){
+                if(String.valueOf(chave.charAt(c)).equalsIgnoreCase(m[i][0])){
+                    x = i;
+                }                                         
+            }
+            for(int j=0; j<27; j++){
+                if(String.valueOf(msg.charAt(c)).equalsIgnoreCase(m[0][j])){
+                    y = j;
+                } 
+            }
+            msgCodificada = msgCodificada + m[x][y];            
+        }
+        System.out.println("Resultado:");
+        System.out.println(msgCodificada.toLowerCase());
+    }
+    
+    public static void main(String[] args) {
+        Vigenere v = new Vigenere();        
+    }
+}
